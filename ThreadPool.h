f71acdb494ea252e05d5f407feb17c55a571f719
ReusTylor <554105237@qq.com>
2023-06-27 17:47:22 +0800

    explain1

--- ThreadPool.h
+++ ThreadPool.h
@@ -14,44 +14,65 @@
 class ThreadPool {
 public:
     ThreadPool(size_t);
+    // 函数模板
     template<class F, class... Args>
     auto enqueue(F&& f, Args&&... args) 
         -> std::future<typename std::result_of<F(Args...)>::type>;
+    // 析构函数
     ~ThreadPool();
 private:
     // need to keep track of threads so we can join them
+    // 存储线程对象
     std::vector< std::thread > workers;
+
     // the task queue
+    // 存储任务的队列，用于保存待执行的任务
     std::queue< std::function<void()> > tasks;
     
     // synchronization
+    // 互斥锁
     std::mutex queue_mutex;
+    // 条件变量用于实现进程同步
     std::condition_variable condition;
+    // 布尔变量，指示线程池是否停止执行任务
     bool stop;
 };
  
 // the constructor just launches some amount of workers
+// 根据给定的线程数创建一个线程池，并初始化成变量
 inline ThreadPool::ThreadPool(size_t threads)
     :   stop(false)
 {
     for(size_t i = 0;i<threads;++i)
         workers.emplace_back(
+            // this捕获当前对象的指针
             [this]
             {
+                // 无限循环 表示线程将一直运行，直到收到停止信号
                 for(;;)
                 {
+                    // 声明一个可调用对象的便来嗯，用于存储从任务队列中获取的任务
                     std::function<void()> task;
 
                     {
+                        // 创建一个独占的互斥锁，用于对任务队列的访问进行同步
                         std::unique_lock<std::mutex> lock(this->queue_mutex);
+
+                        // 线程在此处等待，直到以下条件之一发生：
+                        // 1. stop为true，表明线程池已经停止
+                        // 2. 任务队列不为空,表示有任务可以执行
                         this->condition.wait(lock,
                             [this]{ return this->stop || !this->tasks.empty(); });
+                        // 检查线程池是否已停止且任务队列为空，如果是，则结束当前线程的执行。
                         if(this->stop && this->tasks.empty())
                             return;
+                        // 从任务队列中获取一个任务，并使用 std::move 进行移动语义的操作。
+                        // 将任务队列中的第一个任务进行移动操作，将其所有权转移到 task 对象中。
                         task = std::move(this->tasks.front());
+                        // 移除队列的头部元素，即已经获取的任务
                         this->tasks.pop();
                     }
-
+                    // 执行获取到的任务，即调用可调用对象的 operator()。
                     task();
                 }
             }
@@ -59,16 +80,21 @@ inline ThreadPool::ThreadPool(size_t threads)
 }
 
 // add new work item to the pool
+// 函数模板，用于向任务队列中添加新的任务。
+// 函数模板的目的是能够接受任意类型的可调用对象（如函数指针，函数对象，lamda表达式等）和对应的参数
 template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args) 
     -> std::future<typename std::result_of<F(Args...)>::type>
 {
+    // using 关键字定义别名
     using return_type = typename std::result_of<F(Args...)>::type;
 
+    // 创建了一个 std::packaged_task 对象，封装了需要执行的任务
     auto task = std::make_shared< std::packaged_task<return_type()> >(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
         );
-        
+    
+    // 获取与packaged_task关联的std::future对象，用于获取任务执行的结果
     std::future<return_type> res = task->get_future();
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
@@ -76,10 +102,12 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
         // don't allow enqueueing after stopping the pool
         if(stop)
             throw std::runtime_error("enqueue on stopped ThreadPool");
-
+        // 将任务封装成lamda函数，并存储在任务队列tasks中
         tasks.emplace([task](){ (*task)(); });
     }
+    // 通知一个等待中的进程，表示有新任务可用。
     condition.notify_one();
+    // 返回之前获取的future对象，以便用户可以在需要时获取任务的执行结果
     return res;
 }
 
--- explain.md
+++ explain.md
@@ -21,3 +21,114 @@
     condition.notify_all();  // 唤醒所有等待中的线程
     ```
     在线程池的代码中，`std::condition_variable` 被用于线程的同步。当任务队列为空时，线程会调用 `condition.wait()` 进入阻塞状态，等待新任务的到来。当有新任务被添加到队列时，通过 `condition.notify_one()` 唤醒一个等待中的线程，使其开始执行任务。
+
+
+
+### 函数模板
+
+
+
+
+
+### lamda函数
+lambda函数的语法是[] () { ... }
+```C++
+// 捕获当前对象的指针
+[this]
+// 这里()为空，表示函数参数列表为空
+{
+    for(;;)
+    {
+        std::function<void()> task;
+
+        {
+            std::unique_lock<std::mutex> lock(this->queue_mutex);
+            this->condition.wait(lock,
+                [this]{ return this->stop || !this->tasks.empty(); });
+            if(this->stop && this->tasks.empty())
+                return;
+            task = std::move(this->tasks.front());
+            this->tasks.pop();
+        }
+
+        task();
+    }
+}
+
+```
+
+
+
+
+### 移动语义
+
+
+
+### using关键字定义别名
+
+
+
+
+### 智能指针
+1. unique_ptr<T> 独占资源所有权的指针
+2. shared_ptr<T> 共享资源所有权的指针
+3. weak_ptr<T> 共享资源的观察者，需要和std::shared_ptr 一起使用，不影响资源的生命周期。
+   
+#### std::unique_ptr
+当独占资源的所有权时，可以使用std::unique_ptr对资源进行管理——离开 unique_ptr 对象的作用域时，会自动释放资源。这是很基本的 RAII 思想。
+
+1. 使用裸指针，要记得释放内存
+   ```C++
+   {
+    int* p = new int(100);
+    // ... 
+    delete p;
+   }
+   ```
+2. 使用unique_ptr 自动管理内存
+    ```C++
+    {
+        std::unique_ptr<int> uptr = std::make_unique(200);
+    }
+
+    ```
+3. move-only
+4. 指向一个数组
+    ```C++
+    {
+        std::unique_ptr<int[]> uptr = std::make_unique<int[]>(10);
+        for (int i = 0; i < 10; i++) {
+            uptr[i] = i * i;
+        }   
+        for (int i = 0; i < 10; i++) {
+            std::cout << uptr[i] << std::endl;
+        }   
+    }
+    ```
+
+
+
+#### shared_ptr
+    对资源作引用计数--当计数为0时，自动释放资源
+### std::result_of()
+用于获取给定函数类型或函数对象的返回类型
+### std::bind()
+
+
+
+### std::forward()
+
+
+### std::future()
+C++11中的一种异步操作结果的表示对象，提供了一种机制来访问异步操作的结果或者等待异步操作的完成
+
+std::future 对象表示一个异步操作的未来结果，它可以与异步任务关联，以获取该任务的结果。通过 std::future 对象，可以进行以下操作：
+ 1. 等待异步操作完成：使用 std::future::wait 或 std::future::wait_for 函数等待异步操作的完成。
+ 2. 获取异步操作的结果：使用 std::future::get 函数获取异步操作的结果，该函数会阻塞当前线程，直到异步操作完成并返回结果。
+ 3. 检查异步操作的状态：使用 std::future::valid 函数检查 std::future 对象是否与有效的异步操作关联。
+ 4. 同步获取异步操作的结果：使用 std::future::share 函数将 std::future 对象转换为 std::shared_future 对象，以允许多个线程共享异步操作的结果。
+
+通过 std::future，可以方便地进行异步操作的管理和结果获取，从而实现异步编程和并发控制。
+
+
+### RAII原理
